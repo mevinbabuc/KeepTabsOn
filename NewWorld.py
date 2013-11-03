d7088c216de9714147e6171a842befac2b65fde6
Mevin <mevinbabuc@gmail.com>
2013-11-03 04:06:44 -0500

    Testing templates static files

--- NewWorld.py
+++ NewWorld.py
@@ -1,18 +1,28 @@
 from google.appengine.api import users
 import webapp2
+import jinja2
 
-class MainPage(webapp2.RequestHandler):
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class LandingPage(webapp2.RequestHandler):
 
     def get(self):
         user=users.get_current_user()
+        msg=""
         
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('Hello, World!')
+            msg="Hello, !"+user.nickname+" !"
         else :
             self.redirect(users.create_login_url(self.request.uri))
+        
+        values={ 'msg':msg,   }
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(values))
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', LandingPage),
 ], debug=True)
