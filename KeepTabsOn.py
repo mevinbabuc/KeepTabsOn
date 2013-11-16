d8f637a79fbd49890ea2a1acb8508979c19b46a3
Mevin <mevinbabuc@gmail.com>
2013-11-16 18:00:46 +0530

    final push

--- KeepTabsOn.py
+++ KeepTabsOn.py
@@ -16,8 +16,6 @@ import webapp2
 import jinja2
 import json
 
-import tweepy
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     autoescape=True,
@@ -26,16 +24,9 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
 MISSING_CLIENT_SECRETS_MESSAGE = """
-<h1>Warning: Please configure OAuth 2.0</h1>
+<h1>ERROR: I'm broke now :(</h1>
 <p>
-To make this sample run you will need to populate the client_secrets.json file
-found at:
-</p>
-<p>
-<code>%s</code>.
-</p>
-<p>with information found on the <a
-href="https://code.google.com/apis/console">APIs Console</a>.
+Server Error :(
 </p>
 """ % CLIENT_SECRETS
 
@@ -48,35 +39,15 @@ decorator = appengine.oauth2decorator_from_clientsecrets(
     ],
     message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-
-class HashStore(ndb.Model):
-    """Models an individual HashStore entry with hastag, content, and date."""
-    hastag = ndb.StringProperty(indexed=True)
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-
-class Note(ndb.Model):
-    """Models an individual Note entry."""
-    author = ndb.UserProperty()
-    title = ndb.StringProperty(indexed=False)
-    content = ndb.StringProperty(indexed=False)
-    hashtag = ndb.StringProperty(repeated=True)
-    last_viewed = ndb.DateTimeProperty(auto_now_add=True)
-
-
-
 class MainHandler(webapp2.RequestHandler):
 
   @decorator.oauth_aware
   def get(self):
-    auth = tweepy.OAuthHandler("i96lx1P2EN4TD8yDf2ayg", "BND5qHEn9LKXtZcsW2PwunWo3Ao3KNzy5c9pUVpJg")
     variables = {
         'google_url': decorator.authorize_url(),
         'google': decorator.has_credentials(),
-        'twitter_url':auth.get_authorization_url()
         }
-    template = JINJA_ENVIRONMENT.get_template('main.html')
+    template = JINJA_ENVIRONMENT.get_template('templates/auth.html')
     self.response.write(template.render(variables))
 
 class login(webapp2.RequestHandler):
@@ -88,51 +59,6 @@ class login(webapp2.RequestHandler):
             self.redirect("/")
         else:
             self.redirect(users.create_login_url(self.request.uri))
-        
-class Add(webapp2.RequestHandler):
-
-  def post(self):
-      NoteTitle = self.request.get("title")
-      NoteHashtags = []
-      NoteHashtags.append(self.request.get("hashtags"))
-      
-      HashEntry=HashStore(hastag=str(repr(NoteHashtags)),content="data")
-      HashEntry.put()
-      
-      NoteEntry = Note(author=users.get_current_user(),hashtag=NoteHashtags,title=NoteTitle)
-      NoteEntry.content="data"
-      NoteEntry.put()
-
-
-class Remove(webapp2.RequestHandler):
-
-  def post(self):
-      noteId = self.request.get("noteId")
-      self.response.write(str(noteId))
-        
-class View(webapp2.RequestHandler):
-
-  def get(self):
-      # noteId = self.request.get("noteId")
-      
-      qry = Note.query().filter(Note.author==users.get_current_user())
-      for temp in qry:
-          self.response.write(str(temp.title)+" "+str(temp.hashtag)+" "+str(temp.author))
-
-class renderPage(webapp2.RequestHandler):
-    
-  def get(self):
-      
-      msg="Hello !"
-      user=users.get_current_user()
-      
-      if user:
-          msg="Hello, "+str(user.nickname())+"!"
-          
-      values={ 'msg':msg,   }
-      template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-      self.response.write(template.render(values))
-
 
 class TagSearch(webapp2.RequestHandler):
   
@@ -168,10 +94,6 @@ application = webapp2.WSGIApplication(
       webapp2.Route(r'/u/', MainHandler),
       webapp2.Route(r'/tag/<orderBy:best|recent>/<query:.*>', TagSearch),
       ('/login', login),
-      ('/render',renderPage),
-      ('/add',Add),
-      ('/remove',Remove),
-      ('/view',View),
       webapp2.Route(decorator.callback_path, decorator.callback_handler()),
     ],
     debug=True)
