62bd805918402928e26e7003f23318cfdaf82ee1
Mevin <mevinbabuc@gmail.com>
2013-12-01 16:44:44 +0530

    Added little sense to the code

--- KeepTabsOn.py
+++ KeepTabsOn.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
-#
+
+
+# KeepTabsOn
+# A Sticky note that's driven by internet hashtags :D
+
+# The MIT License (MIT)
+
+# Copyright (c) 2013 Mevin Babu Chirayath @mevinbabuc
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import httplib2
 import logging
@@ -39,15 +64,17 @@ decorator = appengine.oauth2decorator_from_clientsecrets(
         ],
     message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-#Hashtag datastore
 class HashStore(ndb.Model):
     """Models an individual HashStore entry with hastag, tile, and date."""
+
     author = ndb.UserProperty()
     title = ndb.StringProperty(indexed=False)
     hashtag = ndb.StringProperty(indexed=True, default="")
     viewDate = ndb.DateTimeProperty(auto_now_add=True)
 
 def CSOR_Jsonify(func):
+    """ decorator to make all the requests CSOR compatible and jsonfy the output """
+
     def wrapper(*args, **kw):
 
         dataOject=func(*args, **kw)
@@ -65,15 +92,26 @@ def CSOR_Jsonify(func):
         args[0].response.write(json.dumps(dataOject))
     return wrapper
 
-
-#Rest processing for KeepTabsOn
 class ResT(webapp2.RequestHandler):
+    """ Class to handle requests (GET, POST, DELETE) to the route /tag/ . """
 
-    #add objects to datastore
 
     @CSOR_Jsonify
     @decorator.oauth_aware
     def post(self,query=""):
+        """ Post Request handler to add data to the HashStore
+
+            args   :None
+
+            return :status
+                    status['object'] -> The object that was added to the HashStore with title and hastag
+                    status['success']-> Has a boolean value. -> True | False 
+                    status['error']  -> Error message if any
+
+            Exceptions/response status codes :201 -> Created    -> When a new object was saved in HashStore
+                                              404 -> Not Found  -> When the post variables title and hashtags was blank or NULL
+
+        """
 
         status={}
         status["error"]=None
@@ -82,7 +120,6 @@ class ResT(webapp2.RequestHandler):
 
         NoteTitle = self.request.get("title")
         NoteHashtags = self.request.get("hashtags")
-
 
         if NoteHashtags and NoteTitle:
             HashEntry=HashStore(author=users.get_current_user(),hashtag=NoteHashtags,title=NoteTitle)
@@ -97,12 +134,21 @@ class ResT(webapp2.RequestHandler):
 
         return status
 
-
-    #view the list of objects in datastore
-    
     @CSOR_Jsonify
     @decorator.oauth_aware
     def get(self,query=""):
+        """ Get request handler to retrieve the list of Tabs saved in the HashStore
+
+            args  :None
+
+            return: dataList -> An object containing all the Tabs of the logged in user.Each tab contains title, hashtag
+                                and the date it was created.
+
+            Exceptions/response status codes :404 -> Not Found -> When there's no data in the HashStore for the particular user
+                                              400 -> Bad Request -> When the program is unable to search db etc. Try again later.
+                                              200 -> Ok -> When data is found and proper data is returned.
+
+        """
 
         qry = HashStore.query().filter(HashStore.author==users.get_current_user())
         dataList=[]
@@ -126,16 +172,23 @@ class ResT(webapp2.RequestHandler):
 
         return dataList
 
-    #delete objects from datstore
-
     @CSOR_Jsonify
     @decorator.oauth_aware
     def delete(self,query):
+        """ Delete request handler to delete a Tab from HashStore
+
+            args   :query -> Accepts tabs(Hashtag) that has to be deleted for the particular user
+
+            return :status -> No value -> Delete request is not supposed to return any value
+
+            response status codes :404 -> Not Found -> When the data to be deleted is not found in the HashStore
+                                   204 -> No Content -> When data is found in the HashStore and deleted,so there's no content to return
+                                   400 -> Bad Request -> When invalid query( Hashtag) was passed to the delete request
+
+        """
 
         status={}
-
         hashtags = query.strip()
-
 
         if hashtags:
             qry = HashStore.query().filter(HashStore.author==users.get_current_user(),HashStore.hashtag==hashtags).fetch(keys_only=True)
@@ -144,35 +197,45 @@ class ResT(webapp2.RequestHandler):
             if not qry:
                 self.response.set_status(404,"Not Found")
             else :
-                self.response.set_status(200,"Ok")
+                self.response.set_status(204,"No Content")
 
         if not hashtags:
-            self.response.set_status(204,"No Content")
+            self.response.set_status(400,"Bad Request")
 
         return status
 
     def options(self,query=""):
-        self.response.set_status(200,"Ok")
 
         try:
             _origin = self.request.headers['Origin']
         except:
             _origin = "http://gcdc2013-keeptabson.appspot.com/"
-            
+
+        self.response.set_status(200,"Ok")
         self.response.headers.add_header("Access-Control-Allow-Origin", _origin)
         self.response.headers.add_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
         self.response.headers.add_header("Access-Control-Allow-Credentials", "true")
         self.response.headers.add_header("Access-Control-Allow-Headers", "origin, x-requested-with, content-type, accept")
 
 
-#Rest output for processing for Search Request
 class ResTSearch(webapp2.RequestHandler):
+    """ Class to handle GET request to search google plus using the G+ API. """
 
-    #search function using
-    
+
     @CSOR_Jsonify
     @decorator.oauth_aware
     def get(self,orderBy,query):
+        """ Get request to search google plus for the best and recent results, based on Hashtags.
+
+            args   : orderBy -> accepts two values ,"Best" and "Recent"
+
+            return : TagDataSuper -> Object with search results from GPlus API.
+
+            response status codes : 200 -> Ok -> Found search results for the queries
+                                    404 -> Not Found -> No content found for the query provided
+                                    400 -> Bad Request -> Either of the arguments is not present.Unable to search.
+
+        """
 
         TagDataSuper=[]
         if query:
@@ -211,13 +274,12 @@ class ResTSearch(webapp2.RequestHandler):
 
     def options(self,query="",orderBy=""):
 
-        self.response.set_status(200,"Ok")
-
         try:
             _origin = self.request.headers['Origin']
         except:
             _origin = "http://gcdc2013-keeptabson.appspot.com/"
-            
+        
+        self.response.set_status(200,"Ok")
         self.response.headers.add_header("Access-Control-Allow-Origin", _origin)
         self.response.headers.add_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
         self.response.headers.add_header("Access-Control-Allow-Credentials", "true")
@@ -225,6 +287,7 @@ class ResTSearch(webapp2.RequestHandler):
 
 
 class MainHandler(webapp2.RequestHandler):
+
 
     @decorator.oauth_aware
     def get(self):
@@ -237,6 +300,7 @@ class MainHandler(webapp2.RequestHandler):
 
 class login(webapp2.RequestHandler):
 
+
     def get(self):
         user = users.get_current_user()
 
@@ -244,6 +308,7 @@ class login(webapp2.RequestHandler):
             self.redirect("/")
         else:
             self.redirect(users.create_login_url(self.request.uri))
+
 
 application = webapp2.WSGIApplication(
     [
