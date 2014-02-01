bb16debb1c9a143915f9a490b06f853fe9162781
Mevin <mevinbabuc@gmail.com>
2014-02-01 11:54:45 +0530

    Oauth template fix and clean up

--- KeepTabsOn.py
+++ KeepTabsOn.py
@@ -22,21 +22,25 @@ import httplib2
 import logging
 import os
 import re
+import json
+
+
+from google.appengine.api import memcache
+from google.appengine.api import users
+from google.appengine.ext import ndb
+# from google.appengine.ext.webapp import RequestHandler, template
 
 from apiclient import discovery
 from oauth2client import appengine
 from oauth2client import client
-from google.appengine.api import memcache
-from google.appengine.api import users
-from google.appengine.ext import ndb
 
 import webapp2
+from webapp2_extras import sessions
 import jinja2
-import json
 
 import sys
 sys.path.insert(0, 'tweepy/')
-import twitsrv.handlers
+import tweepy
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -60,6 +64,26 @@ decorator = appengine.oauth2decorator_from_clientsecrets(
         'https://www.googleapis.com/auth/plus.me',
         ],
     message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+
+class SessionHandler(webapp2.RequestHandler):
+
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session(backend="datastore")
+
+class OAuthToken(ndb.Model):
+    """Model to hold Oauth token for GPlus"""
+
+    token_key = ndb.StringProperty(required=True)
+    token_secret = ndb.StringProperty(required=True)
 
 class HashStore(ndb.Model):
     """Models an individual HashStore entry with hastag, tile, and date."""
@@ -240,7 +264,7 @@ class ResT(webapp2.RequestHandler):
         self.response.headers.add_header("Access-Control-Allow-Headers",
          "origin, x-requested-with, content-type, accept")
 
-
+#/tag/best/<Query>
 class ResTSearch(webapp2.RequestHandler):
     """Class to handle GET request to search google plus using the G+ API. """
 
@@ -308,7 +332,9 @@ class ResTSearch(webapp2.RequestHandler):
 
         return TagDataSuper
 
+###############################################################################
 
+#/g/best/<Query>
 class GplusSearch(webapp2.RequestHandler):
     """Class to handle GET request to search google plus using the G+ API. """
 
@@ -398,12 +424,128 @@ class GplusOauth(webapp2.RequestHandler):
 
     @decorator.oauth_aware
     def get(self):
-        variables = {
+        context = {
             'google_url': decorator.authorize_url(),
             'google': decorator.has_credentials(),
+            'twitter_url':None,
         }
         template = JINJA_ENVIRONMENT.get_template('templates/auth.html')
-        self.response.write(template.render(variables))
+        self.response.write(template.render(context))
+
+###############################################################################
+
+
+CONSUMER_KEY = 'heqMmsf4eLA8RtmyIhu1w'
+CONSUMER_SECRET = '7SNHt57hQVmT6O9yaiFY1m5jjSO4o6t5x0A1Ll65Tg'
+# CALLBACK = 'http://gcdc2013-keeptabson.appspot.com/twitoauth/callback'
+CALLBACK = 'http://127.0.0.1:8080/twitoauth/callback'
+
+# OAuth request handler  (/twitoauth/)
+class TwitOauth(webapp2.RequestHandler):
+
+    def get(self):
+        context = {}
+        # Build a new oauth handler and display authorization url to user.
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK)
+        try:
+            context = {
+
+                    "twitter_url": auth.get_authorization_url(),
+                    'google_url': None,
+                    'google': None,
+            }
+        except tweepy.TweepError, e:
+            # Failed to get a request token
+            return self.response.write("Tweepy error"+str(e))
+
+        # We must store the request token for later use in the callback page.
+        request_token = OAuthToken(
+                token_key = auth.request_token.key,
+                token_secret = auth.request_token.secret
+        )
+        request_token.put()
+
+        template = JINJA_ENVIRONMENT.get_template('templates/auth.html')
+        self.response.write(template.render(context))
+
+# Callback page (/twitoauth/callback)
+class CallbackPage(SessionHandler):
+
+    def get(self):
+        oauth_token = self.request.get("oauth_token", None)
+        oauth_verifier = self.request.get("oauth_verifier", None)
+        if oauth_token is None:
+            # Invalid request!
+            return self.response.write("invalid callback request")
+
+        # Lookup the request token
+        request_token = OAuthToken.gql("WHERE token_key=:key", key=oauth_token).get()
+        if request_token is None:
+            # We doautho not seem to have this request token, show an error.
+            return self.response.write("Invalid token.Token not in db")
+
+        # Rebuild the auth handler
+        _OAUTHobj = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        _OAUTHobj.set_request_token(request_token.token_key, request_token.token_secret)
+
+        # Fetch the access token
+        try:
+            _OAUTHobj.get_access_token(oauth_verifier)
+        except tweepy.TweepError, e:
+            # Failed to get access token
+            return self.response.write("Tweepy error"+str(e))
+
+        self.session['_OAUTHobj'] = _OAUTHobj
+        
+        self.response.write("Twitter Authorized. Now you can enjoy the power of twitter too.")
+
+#/t/best/<Query>
+class TwitterSearch(SessionHandler):
+
+    def get(self,orderBy,query="No Data"):
+        _OAUTHobj = self.session.get('_OAUTHobj')
+
+        if _OAUTHobj:
+            api = tweepy.API(_OAUTHobj)
+
+            if orderBy == 'best':
+                orderBy = 'popular'
+            search_result = api.search(q=query,result_type=orderBy)
+
+            TagDataSuper=[]
+
+            for each in search_result:
+                dataObject={}
+                dataObject["post_url"] = "https://twitter.com/"+each.user.screen_name+"/status/"+each.id_str
+                dataObject["title"] = re.sub(r'#[\S]+|(http|https)://[\S]+|@[\S]+','',each.text).strip()
+                dataObject["date"] = each.created_at.strftime("%Y/%m/%d %H:%M")
+                dataObject["user"] = each.user.screen_name
+                dataObject["user_url"] = "https://twitter.com/account/redirect_by_id/"+each.user.id_str
+                dataObject["user_img_url"] = each.user.profile_image_url
+                dataObject['content']= each.text
+
+                URLdic={}
+                URLStack = []
+                for each_url in each.entities['urls']:
+                    URLStack.append(each_url['expanded_url'])
+                URLdic['turl'] = URLStack
+                dataObject["attached_content"] = URLdic
+
+                metadata={}
+                metadata['retweet'] = each.retweet_count
+                metadata['favourites_count'] = each.favorite_count
+                metadata['verified'] = each.user.verified
+                dataObject['metadata'] = metadata
+
+                TagDataSuper.append(dataObject)
+
+            self.response.headers.add_header('Content-Type', 'application/json')
+            self.response.set_status(200,"Ok")
+            return self.response.write(json.dumps(TagDataSuper))
+        else:
+            return self.redirect('/twitoauth/')
+
+###############################################################################
 
 class login(webapp2.RequestHandler):
 
@@ -417,7 +559,7 @@ class login(webapp2.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
 
 config={}
-config['webapp2_extras.sessions'] = {'secret_key': 'my-super-secret-key',}
+config['webapp2_extras.sessions'] = {'secret_key': 'this~is!my#session^super*key',}
 
 application = webapp2.WSGIApplication(
     [
@@ -431,9 +573,9 @@ application = webapp2.WSGIApplication(
 
 
         # OAuth for twitter
-        (r'/twitoauth/', twitsrv.handlers.TwitOauth),
-        (r'/twitoauth/callback', twitsrv.handlers.CallbackPage),
-        webapp2.Route(r'/t/<orderBy:best|recent>/<query:.*>', twitsrv.handlers.TwitterSearch),
+        (r'/twitoauth/', TwitOauth),
+        (r'/twitoauth/callback', CallbackPage),
+        webapp2.Route(r'/t/<orderBy:best|recent>/<query:.*>', TwitterSearch),
 
         webapp2.Route(decorator.callback_path, decorator.callback_handler()),
         ],
